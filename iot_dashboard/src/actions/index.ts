d08f1977db727476bd1d12e6e5b4fb9f808db032
KarunaShrestha02 <karuna100502@gmail.com>
2024-08-27 16:46:40 +0545

    no message

--- iot_dashboard/src/actions/index.ts
+++ iot_dashboard/src/actions/index.ts
@@ -2,7 +2,7 @@
 import axios from "@/config/axiosTb";
 import { AxiosResponse } from "axios";
 import { redirect } from "next/navigation";
-import createSupabaseServerClient from "@/lib/supabase/server";
+import createSupabaseServerClient from "@/lib/supabase/server"; //"@/lib/supabase/server";
 import { revalidatePath,unstable_noStore as noStore } from "next/cache";
 
 type ResponseData = {
